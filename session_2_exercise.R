8ac915fb89b5ba3c568c6377c1b7bbcccdb8d399
LorenzoCecconi <lorenzo.cecconi@mail.polimi.it>
2023-05-01 19:58:43 +0200

    ex3

--- session_2_exercise.R
+++ session_2_exercise.R
@@ -1,2 +1,47 @@
 x <- runif(100, 1, 100)
 mean_x <- round(mean(x), 2)
+
+x <- c(1:1000)
+x <- seq(1,1000,1)
+y <- 1000-x
+dollars <- sum(x+y)
+
+n <- 1e4
+scale <- 1.5e4
+income <- round( rbeta(n=n, shape1=2, shape2=14) * scale, 2)
+mean(income)
+library(ggplot2) # only load (run) once
+
+# Plot the resulting curve
+ggplot(data.frame(x = income), aes(x=x)) +
+  geom_histogram(color = "#0065BD", fill = "#0065BD", alpha=0.5, bins = 100) +
+  scale_x_continuous(breaks = seq(0, scale, 1e3)) + 
+  labs(x = "Gross income", 
+       y = "Counts") + 
+  theme_minimal()
+
+income_s <- sort(income)
+group <- c("Lower 1%", "Lower 50%", "Top 10%", "Top 1%")
+p <- c(.1, .5, .9, .99)
+
+boundary <- round(income_s[p*n], 0)
+
+low10_m <- mean( income_s[c(1:(.1*n))] )
+low50_m <- mean( income_s[c(1:(.5*n))] )
+top10_m <- mean( income_s[c((.9*n):n)] )
+top1_m <- mean( income_s[c((.99*n):n)] )
+
+means <-  round( c(low10_m, low50_m, top10_m, top1_m) , 0)
+
+income_summary <- data.frame(group, boundary, means)
+income_summary
+
+##       group boundary means
+## 1  Lower 1%      618   398
+## 2 Lower 50%     1865  1073
+## 3   Top 10%     4014  4979
+## 4    Top 1%     6125  6737
+
+##Ex.3
+x <- matrix(data=round(runif(n,min = 1,max = 100)), nrow = 5, ncol = 5)
+mean <- mean
